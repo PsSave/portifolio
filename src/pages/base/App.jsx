3dca4d45274ad69505bdd9c1916eb6d40f98a9b2
Pedro Samuel <pedrosviki1265@gmail.com>
2023-12-22 18:55:55 -0400

    first commit

---
+++ src/pages/base/App.jsx
@@ -0,0 +1,14 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './pages/components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
