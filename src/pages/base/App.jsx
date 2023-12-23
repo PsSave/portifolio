27ab062966a647f300a1f405df48d94f51afc706
Pedro Samuel <pedrosviki1265@gmail.com>
2023-12-22 20:40:23 -0400

    👌 style: ajuste na base e no global css

--- src/pages/base/App.jsx
+++ src/pages/base/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from '../../components/header/header.jsx'
+import Main from '../../components/main_section/main_section.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
