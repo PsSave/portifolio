a92bbd8d61172705d20d9e37ec71013e4c514c32
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-07-06 12:00:51 -0300

    ✨feat: Adicionar primeira parte do portfolio, profile.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,38 @@
 const intro = document.getElementsByClassName("intro")[0];
 const btn_intro = document.getElementById("btn_intro");
 const portfolio = document.getElementById("portfolio");
+const listSocial = document.getElementsByClassName("list_social");
 
-btn_intro.addEventListener("click", handleOpenPortfolio);
+if (btn_intro) {
+  btn_intro.addEventListener("click", handleOpenPortfolio);
+}
+
+Array.from(listSocial).forEach((item) => {
+  item.addEventListener("mouseover", handleAlterColorSocialIcon);
+  item.addEventListener("mouseout", handleResetColorSocialIcon);
+});
+
+function handleAlterColorSocialIcon(event) {
+  const svg = event.currentTarget.querySelector(".social-svg");
+  if (svg) {
+    const path = svg.querySelector("path");
+    if (path) {
+      path.setAttribute("fill", "#FF7052");
+    }
+  }
+}
+
+function handleResetColorSocialIcon(event) {
+  const svg = event.currentTarget.querySelector(".social-svg");
+  if (svg) {
+    const path = svg.querySelector("path");
+    if (path) {
+      path.setAttribute("fill", "white"); // Reset to original color
+    }
+  }
+}
 
 function handleOpenPortfolio() {
   intro.style.display = "none";
-  portfolio.style.display = "block";
+  portfolio.style.display = "flex";
 }
